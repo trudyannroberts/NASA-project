e5f73ea78fc656f068470075470e251c51759f75
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-11-27 19:36:09 +0100

    refactor names and fix workflow run files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from src.db.db_mars import create_mars_weather_table, insert_mars_weather
-from src.db.db_asteroids import create_near_earth_objects_table, insert_near_earth_objects
+from src.db.db_mars_weather import create_mars_weather_table, insert_mars_weather
+from src.db.db_neo import create_near_earth_objects_table, insert_near_earth_objects
 from src.db.db_picture import create_space_picture_table, insert_space_picture
 from src.api.api_mars_weather import fetch_mars_data
-from src.api.api_near_earth_objects import fetch_neo_data
+from src.api.api_neo import fetch_neo_data
 from src.api.api_picture import fetch_picture_data
 
 def main():

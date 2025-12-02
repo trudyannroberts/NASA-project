d20f4be0097d4f538373252561577f3fcb8f90f9
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-12-02 09:08:41 +0100

    bug

--- src/api/api_picture.py
+++ src/api/api_picture.py
@@ -14,7 +14,7 @@ def fetch_picture_data():
 
     date = data["date"]
     description = data["explanation"]
-    copyright = data["copyright", None]
+    copyright = data.get["copyright", None]
     url = data["url"]
 
     return date, description, copyright, url
